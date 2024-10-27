cfab6a77d65639433f008948c02c2507dc94ae11
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-10-27 14:51:52 +0600

    added defult msg

--- api/greet.js
+++ api/greet.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 export default function handler(req, res) {
     const uri = "mongodb+srv://rayhanalshorifmongodb:8VznScZWt66XHCJ4@cluster0.hl4xe.mongodb.net/?retryWrites=true&w=majority";
 
-    var message = "";
+    var message = "Defualt messgae";
     mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
