f366e5f885c6af7bd182d1d63c461e792ed6604c
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-10-27 14:47:09 +0600

    is it connected?

--- api/greet.js
+++ api/greet.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+
 export default function handler(req, res) {
+    console.log(mongoose);
     res.status(200).json({ message: 'Hello, welcome to my Express app!' });
 }
