8e8baf2fa2c838908e31c3bb6a8c089d1223937c
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-10-27 14:53:08 +0600

    cng name

--- api/greet.js
+++ api/greet.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 
 export default function handler(req, res) {
-    const uri = "mongodb+srv://rayhanalshorifmongodb:8VznScZWt66XHCJ4@cluster0.hl4xe.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://rayhanalshorifmongodb:8VznScZWt66XHCJ4@cluster0.hl4xe.mongodb.net/?new_record";
 
     var message = "Defualt messgae";
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(uri)
         .then(() => {
             message = "Successfully connected to MongoDB!";
         })
